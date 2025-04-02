1c8d28921c9521f0df328cc153f3663d3382b4c6
josephsatharasi <satharasijosephthimothi@gmail.com>
2025-04-02 13:49:51 +0530

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -45,7 +45,7 @@ const Registration = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://backend-ip1x.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
